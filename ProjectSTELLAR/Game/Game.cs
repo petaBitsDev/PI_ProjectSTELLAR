2feb2363b27493628d9b116fea6a19cd202d9fe5
Melvin D <delpierre@intechinfo.fr>
2018-04-16 12:19:24 +0200

    Not working menu

--- ProjectSTELLAR/Game/Game.cs
+++ ProjectSTELLAR/Game/Game.cs
@@ -16,6 +16,7 @@ namespace ProjectStellar
         public const string WINDOW_TITLE = "Project STELLAR";
         Sprite _backgroundSprite;
         Texture _backgroundTexture = new Texture("./resources/img/83504.png");
+        public Texture[] _menuTextures = new Texture[4];
         int _state;
         Menu _menu;
 
@@ -27,38 +28,44 @@ namespace ProjectStellar
         public override void LoadContent()
         {
             DebugUtility.LoadContent();
+            _menuTextures[0] = new Texture("./resources/img/play.png");
+            _menuTextures[1] = new Texture("./resources/img/play2.png");
+            _menuTextures[2] = new Texture("./resources/img/exit.png");
+            _menuTextures[3] = new Texture("./resources/img/exit2.png");
         }
 
         public override void Initialize()
         {
-            _menu = new Menu(1280, 720);
+            _menu = new Menu(1280, 720, this);
             _backgroundSprite = new Sprite(_backgroundTexture);
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
-            {
-                _menu.Move(Keyboard.Key.Up);
-            }
-            else if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
-            {
-                _menu.Move(Keyboard.Key.Down);
+            //if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+            //{
+            //    _menu.Move(Keyboard.Key.Up);
+            //}
+            //else if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+            //{
+            //    _menu.Move(Keyboard.Key.Down);
 
-            }
-            else if (Keyboard.IsKeyPressed(Keyboard.Key.Return))
-            {
-                if (_menu.SelectedItem == 0)
-                {
-                    Window.Close();
-                    Game game = new Game();
-                    game.Run();
-                }
-                else if (_menu.SelectedItem == 1)
-                {
-                    Window.Close();
-                }
-            }
+            //}
+            //else if (Keyboard.IsKeyPressed(Keyboard.Key.Return))
+            //{
+            //    if (_menu.SelectedItem == 0)
+            //    {
+            //        Window.Close();
+            //        //Game game = new Game();
+            //        //game.Run();
+            //    }
+            //    else if (_menu.SelectedItem == 1)
+            //    {
+            //        Window.Close();
+            //    }
+            //}
+            Console.WriteLine(_menu.CheckMouse());
+
         }
 
         public override void Draw(GameTime gameTime)
--- ProjectSTELLAR/Menu.cs
+++ ProjectSTELLAR/Menu.cs
@@ -8,61 +8,95 @@ namespace ProjectStellar
 {
     class Menu
     {
-        readonly Text[] menu = new Text[2];
+        readonly Sprite[] _menu = new Sprite[2];
         static Texture _backgroundTexture = new Texture("./resources/img/83504.png");
         static Sprite _backgroundSprite;
-        private int selectedItem;
+        private int _selectedItem = -1;
         Font font = new Font("./resources/fonts/arial.ttf");
+        Game _ctx;
 
-        public Menu(float width, float height)
+        public Menu(float width, float height, Game ctx)
         {
-            Text menu1 = new Text
-            {
-                Font = font,
-                Color = Color.White,
-                DisplayedString = "Play Game",
-                Position = new Vector2f(width / 2, height / (3 + 1) * 1)
-            };
-            menu[0] = menu1;
+            //Text menu1 = new Text
+            //{
+            //    Font = font,
+            //    Color = Color.White,
+            //    DisplayedString = "Play Game",
+            //    Position = new Vector2f(width / 2, height / (3 + 1) * 1)
+            //};
+            //menu[0] = menu1;
 
-            Text menu2 = new Text
+            //Text menu2 = new Text
+            //{
+            //    Font = font,
+            //    Color = Color.White,
+            //    DisplayedString = "Exit Game",
+            //    Position = new Vector2f(width / 2, height / (3 + 1) * 2)
+            //};
+            //menu[1] = menu2;
+            _ctx = ctx;
+
+            Sprite button1 = new Sprite(_ctx._menuTextures[0])
             {
-                Font = font,
-                Color = Color.White,
-                DisplayedString = "Exit Game",
-                Position = new Vector2f(width / 2, height / (3 + 1) * 2)
+                Position = new Vector2f((width / 5) * 2, height / (3 + 1) * 1),
+                Scale = new Vector2f(0.1f, 0.1f),
             };
-            menu[1] = menu2;
+            _menu[0] = button1;
+
+            Sprite button2 = new Sprite(_ctx._menuTextures[2])
+            {
+                Position = new Vector2f((width / 5) * 2, height / (3 + 1) * 2),
+                Scale = new Vector2f(0.1f, 0.1f),
+            };
+            _menu[1] = button2;
         }
 
         public int SelectedItem
         {
-            get { return selectedItem; }
+            get { return _selectedItem; }
         }
 
         public void Draw(RenderWindow window)
         {
             for (int i = 0; i < 2; i++)
             {
-                window.Draw(menu[i]);
+                window.Draw(_menu[i]);
             }
+        }
+
+        public int CheckMouse()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (_menu[i].GetLocalBounds().Contains((float)Mouse.GetPosition().X, (float)Mouse.GetPosition().Y))
+                {
+                    //if (i != _selectedItem)
+                    //{
+                    //    _menu[i].Texture = _ctx._menuTextures[i + 1];
+                    //    return true;
+                    //}
+                    return (i);
+                }
+                
+            }
+            return -1;
         }
 
         public void Move(Keyboard.Key key)
         {
             if (key == Keyboard.Key.Up)
             {
-                menu[selectedItem].Color = Color.White;
-                selectedItem--;
-                if (selectedItem < 0) selectedItem = 0;
-                menu[selectedItem].Color = Color.Yellow;
+                _menu[_selectedItem].Color = Color.White;
+                _selectedItem--;
+                if (_selectedItem < 0) _selectedItem = 0;
+                _menu[_selectedItem].Color = Color.Yellow;
             }
             else if (key == Keyboard.Key.Down)
             {
-                menu[selectedItem].Color = Color.White;
-                selectedItem++;
-                if (selectedItem > 1) selectedItem = 1;
-                menu[selectedItem].Color = Color.Yellow;
+                _menu[_selectedItem].Color = Color.White;
+                _selectedItem++;
+                if (_selectedItem > 1) _selectedItem = 1;
+                _menu[_selectedItem].Color = Color.Yellow;
             }
         }
     }
