d480a90df168703d0721f1d7e3781442e9506779
Melvin D <delpierre@intechinfo.fr>
2018-04-23 15:56:39 +0200

    Working In Game Time
    In Game Time can now be controlled with TimeScale property

--- ProjectSTELLAR/Game/Game.cs
+++ ProjectSTELLAR/Game/Game.cs
@@ -22,6 +22,8 @@ namespace ProjectStellar
         uint _windowX;
         uint _windowY;
         Map _ctx;
+        UI _ui;
+        Font _font;
 
         public Game(int state, uint windowX, uint windowY, bool isFullscreen) : base(windowX, windowY, isFullscreen, WINDOW_TITLE, Color.Green)
         {
@@ -38,12 +40,15 @@ namespace ProjectStellar
             _menuTextures[1] = new Texture("./resources/img/menuQuit.png");
             _menuTextures[2] = new Texture("./resources/img/menuPlayActif.png");
             _menuTextures[3] = new Texture("./resources/img/menuQuitActif.png");
+
+            _font = new Font("./resources/fonts/arial.ttf");
         }
 
         public override void Initialize()
         {
             _menu = new Menu(_windowX, _windowY, this);
             _backgroundSprite = new Sprite(_backgroundTexture);
+            _ui = new UI();
         }
 
         public override void Update(GameTime gameTime)
@@ -57,7 +62,11 @@ namespace ProjectStellar
             MapUI map = new MapUI(_ctx, 10, 10);
             _backgroundSprite.Draw(Window, RenderStates.Default);
             if (MenuState == 0) _menu.Draw(Window);
-            else if (MenuState == 1) map.RenderGraphics(Window);
+            else if (MenuState == 1)
+            {
+                map.RenderGraphics(Window);
+                _ui.Draw(Window, _font, gameTime.InGameTime);
+            }
         }
 
         public int MenuState
