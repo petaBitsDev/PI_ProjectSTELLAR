283aaf65a2c73e064a554c958c0c44de8ac508aa
Melvin D <delpierre@intechinfo.fr>
2018-05-28 11:02:34 +0200

    Start of BuildingType

---
+++ ProjectSTELLAR.Library/Building/Types/BuildingType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectStellar.Library.Building
+{
+    public abstract class BuildingType
+    {
+        public abstract void CreateInstance(int x, int y);
+
+        public abstract void DeleteInstance(int x, int y);
+        
+        public abstract int Count();
+    }
+
+}
