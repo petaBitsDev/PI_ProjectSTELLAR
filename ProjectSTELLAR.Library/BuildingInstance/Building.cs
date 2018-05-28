86f9d6173521d644d6e62400073c167aa42bcfdb
Kimor <agopian@intechinfo.fr>
2018-05-28 11:05:53 +0200

    Auto stash before merge of "RefactoringBuilding" and "origin/RefactoringBuilding"
    building instance

---
+++ ProjectSTELLAR.Library/BuildingInstance/Building.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectStellar.Library.BuildingInstance
+{
+    class Building 
+    {
+        BuildingType _buildingType;
+        float _x;
+        float _y;
+        public Building(BuildingType buildingType, float x, float y)
+        {
+            _buildingType = buildingType;
+            _x = x;
+            _y = y;
+        }
+    }
+}
