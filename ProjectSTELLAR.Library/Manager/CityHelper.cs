359a532187a6ea6ba5d4cab6d0440cb3160859f1
Arthur C <acheng@intechinfo.fr>
2018-05-28 20:19:21 +0200

    cityhelper and manager

--- ProjectSTELLAR.Library/Manager/CityHelper.cs
+++ ProjectSTELLAR.Library/Manager/CityHelper.cs
@@ -9,53 +9,42 @@ namespace ProjectStellar.Library
     [Serializable]
     public class CityHelper
     {
-       MetalMine metalMine;
-       OreMine oreMine;
-       SawMill sawMill;
-       CityHall cityHall;
-       FireStation fireStation ;
-       Hospital hospital;
-       PoliceStation policeStation ;
-       PowerPlant powerPlant;
-       PumpingStation pumpingStation ;
-       SpaceStation spaceStation;
-       Warehouse warehouse ;
-       Flat flat ;
-       House house ;
-       Hut hut;
-
+        MetalMineType metalMine;
+        SawmillType sawMill;
+        OreMineType oreMine;
+        CityHallType cityHall;
+        FireStationType fireStation;
+        HospitalType hospital;
+        PoliceStationType policeStation;
+        PowerPlantType powerPlant;
+        PumpingStationType pumpingStation;
+        SpaceStationType spaceStation;
+        WarehouseType warehouse;
+        FlatType flat;
+        HouseType house;
+        HutType hut;
         Map _map;
-       public CityHelper(Map map)
-       {
-           _map = map;
+        public CityHelper(Map map)
+        {
+            _map = map;
 
-           metalMine = new MetalMine(_map, 70);
-           oreMine = new OreMine(_map, 70);
-           sawMill = new Sawmill(_map, 70);
-           cityHall = new CityHall(_map);
-           fireStation = new FireStation(_map);
-           hospital = new Hospital(_map);
-           policeStation = new PoliceStation(_map);
-           powerPlant = new PowerPlant(_map, 220);
-           pumpingStation = new PumpingStation(_map, 250);
-           spaceStation = new SpaceStation(_map);
-           warehouse = new Warehouse(_map);
-           flat = new Flat(_map);
-           house = new House(_map);
-           hut = new Hut(_map);
-
-
+            metalMine = new MetalMineType();
+            oreMine = new OreMineType();
+            sawMill = new SawmillType();
+            cityHall = new CityHallType();
+            fireStation = new FireStationType();
+            hospital = new HospitalType();
+            policeStation = new PoliceStationType();
+            powerPlant = new PowerPlantType();
+            pumpingStation = new PumpingStationType();
+            spaceStation = new SpaceStationType();
+            warehouse = new WarehouseType();
+            flat = new FlatType();
+            house = new HouseType();
+            hut = new HutType();
         }
 
         static List<BuildingType> _listBuilding = new List<BuildingType>();
-
-        public List<BuildingType> ListBuilding
-        {
-            get
-            {
-                return _listBuilding;
-            }
-        }
 
         public void CreateListBuilding()
         {
@@ -75,172 +64,71 @@ namespace ProjectStellar.Library
             _listBuilding.Add(hut);
         }
 
-         public MetalMine GetMetalMine
+        public List<BuildingType> ListBuilding
         {
             get
             {
-                foreach(BuildingType b in _listBuilding)
-                {
-                    if (b is MetalMine metalMine) return metalMine;
-                }
-                return null;
+                return _listBuilding;
             }
         }
 
-         public OreMine GetOreMine
+        public HutType GetHut
         {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is OreMine oreMine) return oreMine ;
-                }
-                return null;
-            }
+            get { return hut; }
         }
 
-         public Sawmill GetSawmill
+        public HouseType GetHouse
         {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is Sawmill sawmill) return sawmill;
-                }
-                return null;
-            }
+            get { return house; }
+        }
+        public FlatType GetFlat
+        {
+            get { return flat; }
+        }
+        public WarehouseType GetWarehouse
+        {
+            get { return warehouse; }
+        }
+        public SpaceStationType GetSpaceStation
+        {
+            get { return spaceStation; }
+        }
+        public PumpingStationType GetPumpingStation
+        {
+            get { return pumpingStation; }
+        }
+        public FireStationType GetFireStation
+        {
+            get { return fireStation; }
+        }
+        public PoliceStationType GetPoliceStation
+        {
+            get { return policeStation; }
+        }
+        public HospitalType GetHospital
+        {
+            get { return hospital; }
+        }
+        public PowerPlantType GetPowerPlant
+        {
+            get { return powerPlant; }
+        }
+        public OreMineType GetOreMine
+        {
+            get { return oreMine; }
+        }
+        public MetalMineType GetMetalMine
+        {
+            get { return metalMine; }
         }
 
-         public CityHall GetCityHall
+        public SawmillType GetSawmill
         {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is CityHall cityHall) return cityHall;
-                }
-                return null;
-            }
+            get { return sawMill; }
         }
 
-         public FireStation GetFireStation
+        public CityHallType GetCityHall
         {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is FireStation fireStation) return fireStation;
-                }
-                return null;
-            }
-        }
-
-         public Hospital GetHospital
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is Hospital hospital) return hospital;
-                }
-                return null;
-            }
-        }
-
-         public PoliceStation GetPoliceStation
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is PoliceStation policeStation) return policeStation;
-                }
-                return null;
-            }
-        }
-
-         public PowerPlant GetPowerPlant
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is PowerPlant powerPlant) return powerPlant;
-                }
-                return null;
-            }
-        }
-
-         public PumpingStation GetPumpingStation
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is PumpingStation pumpingStation) return pumpingStation;
-                }
-                return null;
-            }
-        }
-
-         public SpaceStation GetSpaceStation
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is SpaceStation spaceStation) return spaceStation;
-                }
-                return null;
-            }
-        }
-
-         public Warehouse GetWareHouse
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is Warehouse warehouse) return warehouse;
-                }
-                return null;
-            }
-        }
-
-         public Flat GetFlat
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is Flat flat ) return flat;
-                }
-                return null;
-            }
-        }
-
-         public House GetHouse
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is House house) return house;
-                }
-                return null;
-            }
-        }
-
-         public Hut GetHut
-        {
-            get
-            {
-                foreach (BuildingType b in _listBuilding)
-                {
-                    if (b is Hut hut) return hut;
-                }
-                return null;
-            }
+            get { return cityHall; }
         }
     }
-}
