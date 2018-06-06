127c681e488aff9b3cd486cf0ca424c15865b1af
Baptiste TEXIER <bapt.tex@gmail.com>
2018-06-06 10:26:30 +0200

    Add a new resolution (2048 * 1280) for my computer

--- Launcher/FormLauncher.cs
+++ Launcher/FormLauncher.cs
@@ -40,14 +40,15 @@ namespace ProjectStellar.FormLauncher
             //1920 * 1080
             //2560 * 1440
             //3840 * 2160
-            Resolution[] allResolutions = new Resolution[7];
+            Resolution[] allResolutions = new Resolution[8];
             allResolutions[0] = new Resolution() { X = 640, Y = 360 };
             allResolutions[1] = new Resolution() { X = 1024, Y = 576 };
             allResolutions[2] = new Resolution() { X = 1280, Y = 720 };
             allResolutions[3] = new Resolution() { X = 1366, Y = 768 };
             allResolutions[4] = new Resolution() { X = 1920, Y = 1080 };
-            allResolutions[5] = new Resolution() { X = 2560, Y = 1440 };
-            allResolutions[6] = new Resolution() { X = 3840, Y = 2160 };
+            allResolutions[5] = new Resolution() { X = 2048, Y = 1280 };
+            allResolutions[6] = new Resolution() { X = 2560, Y = 1440 };
+            allResolutions[7] = new Resolution() { X = 3840, Y = 2160 };
 
             for (int i = 0; i < allResolutions.Length; i++)
             {
