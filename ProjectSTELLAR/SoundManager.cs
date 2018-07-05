b051f1db22daea7511895ae56804652c3609134e
Melvin D <delpierre@intechinfo.fr>
2018-07-05 10:03:24 +0200

    Working sounds sliders

--- ProjectSTELLAR/SoundManager.cs
+++ ProjectSTELLAR/SoundManager.cs
@@ -19,7 +19,9 @@ namespace ProjectStellar
         {
             _music = new Music(_path + "YouthfulIllusions.ogg");
             _sounds[0] = new Sound(new SoundBuffer(_path + "build.ogg"));
+            _sounds[0].Volume = 50;
             _sounds[1] = new Sound(new SoundBuffer(_path + "destroy.ogg"));
+            _sounds[1].Volume = 50;
         }
 
         public void StartMusic()
@@ -28,6 +30,39 @@ namespace ProjectStellar
             _music.Volume = 50;
             _music.Play();
         }
+
+        public float MusicVolume
+        {
+            get { return _music.Volume; }
+            set
+            {
+                if (value < 0)
+                    _music.Volume = 0;
+                else if (value > 100)
+                    _music.Volume = 100;
+                else
+                    _music.Volume = value;
+            }
+        }
+
+        public float SFXVolume
+        {
+            get { return _sounds[0].Volume; }
+            set
+            {
+                for (int i = 0; i < _sounds.Length; i++)
+                {
+                    if (value < 0)
+                        _sounds[i].Volume = 0;
+                    else if (value > 100)
+                        _sounds[i].Volume = 100;
+                    else
+                        _sounds[i].Volume = value;
+                }
+            }
+        }
+
+        
 
         public Sound Build => _sounds[0];
         public Sound Destroy => _sounds[1];
